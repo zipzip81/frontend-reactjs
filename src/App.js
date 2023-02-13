9ebad274dc9cdb4cce15be9584b3adf50c2f497c
zipzip81 <tankhimsuan@gmail.com>
2023-02-13 13:48:49 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function callApi() {
+   fetch('https://service-helloworld-1.as.r.appspot.com', { method: 'GET' })
+    .then(data => data.json()) // Parsing the data into a JavaScript object
+    .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+	  <button onClick={callApi}>Call API</button>
+    </div>
+  );
+}
+
+export default App;
