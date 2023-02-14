cbc02d1cf0655f122a668385735802518f672844
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-14 14:42:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ function callApi() {
 }
 
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleClick = () => {
+    fetch(`https://service-helloworld-1.as.r.appspot.com/api/v1/hello/${name}`)
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  };
+	
   return (
     <div className="App">
       <h1>Hello World!</h1>
-	  <button onClick={callApi}>Call API</button>
+      <button onClick={callApi}>Call API</button>
+
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={handleClick}>Submit</button>
+      <p>{message}</p>
     </div>
   );
 }
