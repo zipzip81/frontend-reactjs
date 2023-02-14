4aee63505990a619c9d97f20b6c5986b4200309c
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-14 14:50:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function callApi() {
-   fetch('https://service-helloworld-1.as.r.appspot.com/api/v1/hello/Tan', { method: 'GET' })
-    .then(data => data.json()) // Parsing the data into a JavaScript object
-    .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
-}
 
 function App() {
   const [name, setName] = useState('');
@@ -20,8 +15,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <button onClick={callApi}>Call API</button>
-
       <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={handleClick}>Submit</button>
       <p>{message}</p>
