aa7c3fa65a6e7f407363eb635b5019b608656982
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-21 22:43:11 +0800

    Create mui-button-demo.js

---
+++ src/mui-button-demo.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Button, TextField } from "@material-ui/core";
+
+function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleClick = () => {
+    fetch(`https://dev-backend-springboot.as.r.appspot.com/api/v1/hello/${name}`)
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={handleClick}>Submit</button>
+      <p>{message}</p>
+
+      <TextField label="Name" variant="outlined" onChange={e => setName(e.target.value)}/>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Submit
+      </Button>
+    </div>
+  );
+}
+
+export default App;
